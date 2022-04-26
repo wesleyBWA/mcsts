a3e92ee403b0436ee2de72b484f544951d7b9bd4
Wesley <bwacomputacao@gmail.com>
2022-04-26 09:25:42 -0300

    Criando projeto

---
+++ src/main/java/com/bwacomputacao/mcsts/Mcsts1Application.java
@@ -0,0 +1,13 @@
+package com.bwacomputacao.mcsts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mcsts1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mcsts1Application.class, args);
+	}
+
+}
