11d188b2e94a1aa3ecf916183089b609ae1272d4
Wesley <bwacomputacao@gmail.com>
2022-05-03 13:55:09 -0300

    Ajuste no endpoint/categorias/{id}

--- src/main/java/com/bwacomputacao/mcsts/services/CategoriaService.java
+++ src/main/java/com/bwacomputacao/mcsts/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.bwacomputacao.mcsts.domain.Categoria;
 import com.bwacomputacao.mcsts.repositories.CategoriaRepository;
+import com.bwacomputacao.mcsts.services.exception.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,6 +17,7 @@ public class CategoriaService {
 	
 	public Categoria find(Integer id) {
 		 Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto nao encontrado! id: " + id + ", Tipo: "+ Categoria.class.getName()));
 	}
 }
