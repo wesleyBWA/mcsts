4e1e1c154f5de9288c0202dd0cd81c5b9e6f7078
Wesley <bwacomputacao@gmail.com>
2022-05-04 11:57:51 -0300

    Estado e cidade

--- src/main/java/com/bwacomputacao/mcsts/Mcsts1Application.java
+++ src/main/java/com/bwacomputacao/mcsts/Mcsts1Application.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.bwacomputacao.mcsts.domain.Categoria;
+import com.bwacomputacao.mcsts.domain.Cidade;
+import com.bwacomputacao.mcsts.domain.Estado;
 import com.bwacomputacao.mcsts.domain.Produto;
 import com.bwacomputacao.mcsts.repositories.CategoriaRepository;
+import com.bwacomputacao.mcsts.repositories.CidadeRepository;
+import com.bwacomputacao.mcsts.repositories.EstadoRepository;
 import com.bwacomputacao.mcsts.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -18,7 +22,11 @@ public class Mcsts1Application implements CommandLineRunner {
 	@Autowired
 	private CategoriaRepository categoriaRepository;
 	@Autowired
-	private ProdutoRepository produtoRepository;  
+	private ProdutoRepository produtoRepository;
+	@Autowired
+	private EstadoRepository estadoRepository;
+	@Autowired
+	private CidadeRepository cidadeRepository; 
 	
 	public static void main(String[] args) {
 		SpringApplication.run(Mcsts1Application.class, args);
@@ -43,6 +51,19 @@ public class Mcsts1Application implements CommandLineRunner {
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "Sao Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlandia", est1);
+		Cidade c2 = new Cidade(null, "Sao Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+		
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 	}
 
 }
