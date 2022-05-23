9cefd2a0f30d49013968418cc9db40afc3e7108f
Wesley <bwacomputacao@gmail.com>
2022-05-23 15:58:05 -0300

    Inserindo uma nova Categoria com POST

--- src/main/java/com/bwacomputacao/mcsts/services/CategoriaService.java
+++ src/main/java/com/bwacomputacao/mcsts/services/CategoriaService.java
@@ -21,7 +21,10 @@ public class CategoriaService {
 		 Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto nao encontrado! id: " + id + ", Tipo: "+Categoria.class.getName()));
+	}
 	
-	
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return repo.save(obj);
 	}
 }
