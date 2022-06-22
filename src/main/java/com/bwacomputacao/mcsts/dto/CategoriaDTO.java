bc0ed2882bd5a6d9288cf25ef66f7d73f3725340
Wesley <bwacomputacao@gmail.com>
2022-06-22 19:15:54 -0300

    PUT, DELETE E GET para cliente

--- src/main/java/com/bwacomputacao/mcsts/dto/CategoriaDTO.java
+++ src/main/java/com/bwacomputacao/mcsts/dto/CategoriaDTO.java
@@ -23,6 +23,8 @@ public class CategoriaDTO implements Serializable {
 	@Length(min=5, max=80, message="O tamanho deve ser entre 5 e 80 caracteres")
 	private String nome;
 	
+	
+	
 	public CategoriaDTO() {
 		
 	}
--- src/main/java/com/bwacomputacao/mcsts/services/ClienteService.java
+++ src/main/java/com/bwacomputacao/mcsts/services/ClienteService.java
@@ -1,12 +1,19 @@
 package com.bwacomputacao.mcsts.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.bwacomputacao.mcsts.domain.Cliente;
+import com.bwacomputacao.mcsts.dto.ClienteDTO;
 import com.bwacomputacao.mcsts.repositories.ClienteRepository;
+import com.bwacomputacao.mcsts.services.exception.DataIntegrityException;
 import com.bwacomputacao.mcsts.services.exception.ObjectNotFoundException;
 
 @Service
@@ -17,7 +24,43 @@ public class ClienteService {
 	
 	public Cliente find(Integer id) {
 		 Optional<Cliente> obj = repo.findById(id);
-		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto nao encontrado! id: " + id
-				+ ", Tipo: "+ Cliente.class.getName()));
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto nao encontrado! id: " + id + ", Tipo: "+ Cliente.class.getName()));
+	}
+		
+	
+	public Cliente update(Cliente obj) {
+		Cliente newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Nao e possivel excluir porque ha entidades relacionadas");
+		}
+		
+	}
+	
+	public List<Cliente> findAll(){
+		return repo.findAll();
+	}
+	
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+	    PageRequest pageRequest = PageRequest.of(page, linesPerPage,Direction.valueOf(direction), orderBy);
+	    return repo.findAll(pageRequest);
+	}
+
+	public Cliente fromDTO(ClienteDTO objDto) {
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
+	
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
 	}
 }
