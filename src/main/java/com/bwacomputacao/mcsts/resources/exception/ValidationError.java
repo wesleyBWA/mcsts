45e0d274bc201f6d1e46d92f55c6dc45c3ef251c
Wesley <bwacomputacao@gmail.com>
2022-06-22 15:10:12 -0300

    Personalizando o retorno da validacao

---
+++ src/main/java/com/bwacomputacao/mcsts/resources/exception/ValidationError.java
@@ -0,0 +1,25 @@
+package com.bwacomputacao.mcsts.resources.exception;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends StandardError {
+	private static final long serialVersionUID = 1L;
+	
+	private List<FieldMessage> errors = new ArrayList<>();
+	
+	public ValidationError(Integer status, String msg, long timeStamp) {
+		super(status, msg, timeStamp);
+		
+	}
+
+	public List<FieldMessage> getErros() {
+		return errors;
+	}
+
+	public void addError(String fieldName, String messagem) {
+		errors.add(new FieldMessage(fieldName, messagem));
+		
+	}
+
+}
