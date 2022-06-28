6384c4479da5813faf2156328fc4f5d0da5975b1
Wesley <bwacomputacao@gmail.com>
2022-06-28 17:53:06 -0300

    Validacao customizada email nao repetido na insersao Cliente

--- src/main/java/com/bwacomputacao/mcsts/repositories/ClienteRepository.java
+++ src/main/java/com/bwacomputacao/mcsts/repositories/ClienteRepository.java
@@ -3,10 +3,13 @@ package com.bwacomputacao.mcsts.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.bwacomputacao.mcsts.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
-
+    
+	@Transactional(readOnly=true)
+	Cliente findByEmail(String email);
 }
--- src/main/java/com/bwacomputacao/mcsts/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/bwacomputacao/mcsts/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,21 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.bwacomputacao.mcsts.domain.Cliente;
 import com.bwacomputacao.mcsts.domain.enums.TipoCliente;
 import com.bwacomputacao.mcsts.dto.ClienteNewDTO;
+import com.bwacomputacao.mcsts.repositories.ClienteRepository;
 import com.bwacomputacao.mcsts.resources.exception.FieldMessage;
 import com.bwacomputacao.mcsts.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	
+	@Autowired
+	private ClienteRepository repo;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -32,6 +41,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ invalido"));
 			
 			}
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if (aux != null) {
+			list.add(new FieldMessage("email", "Email ja existente"));
+		}
 
 
 		for (FieldMessage e : list) {
