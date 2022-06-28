c6dc298e2b5370a2de58b84ea56567940c0f0efa
Wesley <bwacomputacao@gmail.com>
2022-06-28 18:25:51 -0300

    Validacao customizada email nao repetido na atulizacao Cliente

--- src/main/java/com/bwacomputacao/mcsts/dto/ClienteDTO.java
+++ src/main/java/com/bwacomputacao/mcsts/dto/ClienteDTO.java
@@ -9,7 +9,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.bwacomputacao.mcsts.domain.Cliente;
+import com.bwacomputacao.mcsts.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
