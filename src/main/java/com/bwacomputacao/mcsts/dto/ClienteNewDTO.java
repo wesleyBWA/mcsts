f59eab640153c01fa313bfeb5958111f61d7cf2e
Wesley <bwacomputacao@gmail.com>
2022-06-28 17:27:40 -0300

    Validacao customizada de Cpf ou Cnpj na insersao Cliente

--- src/main/java/com/bwacomputacao/mcsts/dto/ClienteNewDTO.java
+++ src/main/java/com/bwacomputacao/mcsts/dto/ClienteNewDTO.java
@@ -2,22 +2,49 @@ package com.bwacomputacao.mcsts.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.bwacomputacao.mcsts.services.validation.ClienteInsert;
+
+@ClienteInsert
 public class ClienteNewDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@NotEmpty(message="Prenchimento obrogatorio")
+    @Length(min=5, max=120, message="O tamanho deve ser entre 5 e 120 caracteres")
 	private String nome;
+	
+	@NotEmpty(message="Prenchimento obrogatorio")
+	@Email(message="Email invalido")
 	private String email;
+	
+	@NotEmpty(message="Prenchimento obrogatorio")
+	
 	private String cpfOuCnpj;
+	
 	private Integer tipo;
 	
+	@NotEmpty(message="Prenchimento obrogatorio")
 	private String Logradouro;
+	
+	@NotEmpty(message="Prenchimento obrogatorio")
 	private String numero;
+	
 	private String complemento;
+	
 	private String bairro;
+	
+	@NotEmpty(message="Prenchimento obrogatorio")
 	private String cep;
 	
+	@NotEmpty(message="Prenchimento obrogatorio")
 	private String telefone1;
+	
 	private String telefone2;
+	
 	private String telefone3;
 	
 	private Integer cidadeId;
---
+++ src/main/java/com/bwacomputacao/mcsts/services/validation/ClienteInsertValidator.java
@@ -0,0 +1,44 @@
+package com.bwacomputacao.mcsts.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import com.bwacomputacao.mcsts.domain.enums.TipoCliente;
+import com.bwacomputacao.mcsts.dto.ClienteNewDTO;
+import com.bwacomputacao.mcsts.resources.exception.FieldMessage;
+import com.bwacomputacao.mcsts.services.validation.utils.BR;
+
+public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	@Override
+	public void initialize(ClienteInsert ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+		
+		List<FieldMessage> list = new ArrayList<>();
+
+		if (objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) 
+				&& !BR.isValidCPF(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CPF invalido"));
+			
+			}
+		
+		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) 
+				&& !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CNPJ invalido"));
+			
+			}
+
+
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
